175146500a1684861928be90dd51633310cf81e0
reall <reall@MSI>
2024-11-06 15:10:33 -0700

    user can log in, next step save the cookie on the user session

--- jokeDatabase/src/main.tsx
+++ jokeDatabase/src/main.tsx
@@ -2,9 +2,19 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { AuthProvider } from 'react-oidc-context';
+
+const oidcConfig = {
+  authority: "https://auth.snowse.duckdns.org/realms/advanced-frontend/",
+  client_id: "r-in-class-2",
+  redirect_uri: "http://localhost:5173/",
+};
+
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <AuthProvider {...oidcConfig}>
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  </AuthProvider>
 )
