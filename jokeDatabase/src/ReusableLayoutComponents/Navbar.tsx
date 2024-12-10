a9e8b7e7d5adf251e14d32cca43cde6e7554ed27
reall <reall@MSI>
2024-12-09 23:40:43 -0700

    added icons to navbar

--- jokeDatabase/src/ReusableLayoutComponents/Navbar.tsx
+++ jokeDatabase/src/ReusableLayoutComponents/Navbar.tsx
@@ -1,5 +1,12 @@
 import { NavbarLink } from "./NavbarLink"
 import LoginLogoutButton from "../Authentication/LoginLogoutButton"
+import { SearchIcon } from "../svg/SearchIcon"
+import { HouseIcon } from "../svg/HouseIcon"
+import { AddIcon } from "../svg/AddIcon"
+import { ListIcon } from "../svg/ListIcon"
+import { AuthorsIcon } from "../svg/AuthorsIcon"
+import { PersonIcon } from "../svg/PersonIcon"
+import { InfoIcon } from "../svg/InfoIcon"
 
 export const Navigation = () => {
     return (
@@ -8,13 +15,13 @@ export const Navigation = () => {
                 {/* <div className="nav-item p-3">
                     <Link to="/">Joke of the day</Link>
                 </div> */}
-                <NavbarLink text="Home" path="/" />
-                <NavbarLink text="Search" path="/search" />
-                <NavbarLink text="All jokes" path="/jokes"/>
-                <NavbarLink text="Add a joke" path="/add" />
-                <NavbarLink text="All authors" path="/authors" />
-                <NavbarLink text="My jokes" path="/myjokes"/>
-                <NavbarLink text="How" path="/how" />
+                <NavbarLink text="Home" path="/" icon={<HouseIcon/>}/>
+                <NavbarLink text="Search" path="/search" icon={<SearchIcon/>}/>
+                <NavbarLink text="All jokes" path="/jokes" icon={<ListIcon/>}/>
+                <NavbarLink text="Add a joke" path="/add" icon={<AddIcon/>} />
+                <NavbarLink text="All authors" path="/authors" icon={<AuthorsIcon/>} />
+                <NavbarLink text="My jokes" path="/myjokes" icon={<PersonIcon/>}/>
+                <NavbarLink text="How" path="/how"  icon={<InfoIcon/>}/>
                 <div className="p-2 m-2">
                     <LoginLogoutButton/>
                 </div>
--- jokeDatabase/src/ReusableLayoutComponents/NavbarLink.tsx
+++ jokeDatabase/src/ReusableLayoutComponents/NavbarLink.tsx
@@ -1,14 +1,16 @@
+import { ReactNode } from "react"
 import { Link } from "react-router"
 interface Props {
     text: string,
-    path: string
+    path: string,
+    icon?: ReactNode
 }
 
-export const NavbarLink:React.FC<Props> = ({path, text}) => {
+export const NavbarLink:React.FC<Props> = ({path, text, icon}) => {
     return (
         <>
             <div className="nav-item p-2 m-2">
-                <Link to={path}>{text}</Link>
+                <Link to={path} className="p-2">{icon}{text}</Link>
             </div>
         </>
     )
